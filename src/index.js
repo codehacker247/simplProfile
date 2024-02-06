fcfa60a771f0464491f38b00aded50b02f3531ef
Rohit Kumar <rohitkumarmandal34@gmail.com>
2024-02-06 18:59:26 +0530

    Last change

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
 function App() {
   return (
@@ -35,7 +51,7 @@ function App() {
 }
 
 function Avatar() {
-  return <img src="rohit.jpeg" alt="Rohit Kumar" className="center"></img>;
+  return <img className="avatar" src="rohit.jpeg" alt="Rohit Kumar" />;
 }
 
 function Intro() {
@@ -43,9 +59,9 @@ function Intro() {
     <div>
       <h1>Rohit Kumar</h1>
       <p>
-        Full-stack web developer in Cognizant. When not coding I like to play
-        board games, to cook (and eat), or to just enjoy the Portuguese sun at
-        the beach.
+        I am a Full-stack web developer in Cognizant. When not coding I like to
+        play board games, to cook (and eat), or to just enjoy the Portuguese sun
+        at the beach.
       </p>
     </div>
   );
@@ -54,19 +70,22 @@ function Intro() {
 function SkillList() {
   return (
     <div className="skill-list">
-      <Skill skill="React" emoji="💪" color="blue" />
-      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
-      <Skill skill="JavaScript" emoji="💪" color="yellow" />
-      <Skill skill="Svelte" emoji="👶" color="orangered" />
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skill, color, level }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
   );
 }
